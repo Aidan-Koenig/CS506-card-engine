e3641ac8135b93688bf4115910494dc21c1aefb8
Jeremiah Sihombing <jsihombing@wisc.edu>
2024-04-08 10:01:15 -0500

    updated settings screen

---
+++ frontend/src/components/SettingsScreen/ChangeNameScreen.jsx
@@ -0,0 +1,52 @@
+import React, { useState } from 'react';
+import { Link  } from 'react-router-dom';
+
+/*Set the Username to change when it appears on the screen */
+function ChangeNameScreen(props){
+    const [username, setUsername] = useState('');
+    const [displayName, setDisplayName] = useState(props.name);
+    
+
+    /*If there is input for username, this will communicate to be put on the database*/
+    const handleChange = () => {
+        if(username == '')
+        {
+            alert("new username cannot be blank or empty!");
+        }
+        else{
+            alert("Change is send to backend!Changing username from current: '" + displayName + "' to new: '" + username  + "'.");
+            setDisplayName(username);
+            setUsername('');
+        }
+		
+	};
+    
+    /*This is a format for settings characteristics assuming username has been developed through login */
+    return <>
+        <div>
+            <h2 className='home' style={{fontSize:'80px',textAlign: 'center', marginBottom:120}}>Change Username</h2>
+        </div>
+        <div>
+            <div>
+                <label className='home' style={{fontSize:'30px',marginLeft:25, marginRight:10}}>Current:</label>
+                <label style={{fontSize:'30px',marginBottom:20}}>{displayName}</label>
+            </div>
+            <div>
+                <label className='home' style={{fontSize:'30px',marginLeft:20,marginRight:50}}>New:</label>
+                <input
+					type="text"					
+					value={username}
+                    onChange={(e) => setUsername(e.target.value)}					
+                    style={{fontSize:'30px',marginBottom:30}}
+				/>
+                <button style={{ marginTop:10,marginBottom:20,marginLeft:20,fontSize: '30px' }}  onClick={handleChange}>Change</button>
+            </div>
+            <div>
+                <Link to='/setting' style={{ marginTop:10,marginBottom:20,marginLeft:30,marginRight:40, fontSize: '30px' }} >Back to Setting</Link>
+			</div>
+        </div>
+    </>
+
+}
+
+export default ChangeNameScreen;
---
+++ frontend/src/components/SettingsScreen/ChangeNameScreen.test.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { render, screen } from '@testing-library/react';
+import ChangeNameScreen from "./ChangeNameScreen";
+import { MemoryRouter } from "react-router-dom";
+
+describe('ChangeNameScreen', () => {
+  it('should render ChangeNameScreen with title and screen items correctly', () => {
+	
+	render(<MemoryRouter><ChangeNameScreen /></MemoryRouter>);
+
+	const screenTitle = screen.getByText(/Change Username/i);
+	expect(screenTitle).toBeInTheDocument();
+
+    const currentText = screen.getByText(/Current:/i);
+	expect(currentText).toBeInTheDocument();
+
+    const newText = screen.getByText(/New:/i);
+	expect(newText).toBeInTheDocument();
+
+    const buttonText = screen.getByRole('button', { name: /Change/i });
+	expect(buttonText).toHaveTextContent('Change');
+
+    const backtosettingText = screen.getByText(/Back to Setting/i);
+	expect(backtosettingText).toBeInTheDocument();
+
+  });
+
+});
