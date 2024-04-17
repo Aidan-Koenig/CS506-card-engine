489909538fc5ab6fbdd882f3e890a8b93af42b0e
Michael Corbishley <mcorbishley@wisc.edu>
2024-04-17 18:57:17 -0500

    update LobbyScreen unit tests for new fetch workflow

--- frontend/src/components/LobbyScreen/LobbyScreen.test.jsx
+++ frontend/src/components/LobbyScreen/LobbyScreen.test.jsx
@@ -3,11 +3,34 @@ import { describe, it, expect } from 'vitest';
 import LobbyScreen from './LobbyScreen';
 
 describe('LobbyScreen', () => {
-  it('should render username and player names correctly', () => {
+	it('should render Lobby title player names correctly', async () => {
+		const mockedData = {
+			player1_name: "Player 1",
+			player2_name: "Player 2",
+			player3_name: null,
+			player4_name: "Player 4",
+		};
 
-	render(<LobbyScreen />);
+		const originalFetch = window.fetch;  // Store original fetch function
 
-	const lobbyTitle = screen.getByText(/Loading.../i); // loading symbol for now since it only shows the lobby after request fulfilled
-	expect(lobbyTitle).toBeInTheDocument();
-  });
+		window.fetch = async () => { // Mock fetch
+			return {
+				ok: true,
+				json: () => mockedData,
+			};
+		};
+
+		render(<LobbyScreen selectedGameId="123" />); // Provide selectedGameId
+
+		await new Promise((resolve) => setTimeout(resolve, 0)); // Wait for rendering
+
+		const lobbyTitle = screen.getByText(/Lobby\./i); // Lobby Title
+		expect(lobbyTitle).toBeInTheDocument();
+
+		const playerNames = screen.getAllByText(/Player /i); // Find all player names, need the space because of Players title
+		console.log(playerNames);
+		expect(playerNames.length).toBe(3); // Expect 3 players (excluding null)
+
+		window.fetch = originalFetch; // Restore original fetch function
+	});
 });
